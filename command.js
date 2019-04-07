13c8679fc0de94d8bad909e2d87169bb9e30d5eb
Ricky Flores <35941728+RickyAndreFlores@users.noreply.github.com>
2019-04-07 16:57:47 -0400

    Add files via upload

---
+++ command.js
@@ -0,0 +1,24 @@
+const shell = require("shelljs");
+const fs    = require('fs'); 
+
+var command = "python deep_dream.py "; //basic command; could be python3 on linux 
+var folder = __dirname + "/images/"; // would be whereever images are stored
+	
+
+var user_clicked = true; // this would somehow communicated from front end
+if(user_clicked)
+{ 
+
+	var string; 
+	
+	fs.readdirSync(folder).forEach(file =>
+	{
+
+		full_command = command + "--input_image " + "images/" + file; 
+        	console.log(full_command);
+		shell.exec(full_command);
+
+	});
+
+
+};

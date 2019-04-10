09f1d1a21cb4ca1a6e8d49884e397c358981d314
Nik Ackermann <nik.ackermann@gmail.com>
2019-04-10 19:11:11 -0400

    Added image uploader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { } from 'semantic-ui-react';
+import { FilePond } from "react-filepond";
+import "filepond/dist/filepond.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <FilePond server="/api/v1/imageupload" />
       </div>
     );
   }

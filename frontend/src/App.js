f84d84718619ec2761928876afecc6f831e2b4a8
Robert Crowdis <RobertCrowdis@gmail.com>
2019-04-23 20:30:21 -0400

    feat: frontend design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,15 @@ import "filepond/dist/filepond.min.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <FilePond server="/api/v1/imageupload" />
+      <div className="app-container">
+      <span className="spacer"></span>
+        <div className="upload">
+          <h1>DeepDream AI Project</h1>
+          <p>Simply drag an image below or select one from the browser to begin!</p>
+          <FilePond server="/api/v1/imageupload" />
+          <p>Once it is done processing, take a look in your <span className="makemegreen">outputs</span> folder.</p>
+        </div>
+        <span id="credits">Created by Robert Crowdis, Nik Ackermann, and Ricky Flores</span>
       </div>
     );
   }
